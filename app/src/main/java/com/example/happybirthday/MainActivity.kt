200eb979418932366d6c182984963ae089cd4483
disha9112 <disha9112@gmail.com>
2021-04-15 23:11:55 +0530

    Initial Commit

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kitkat.quizwhiz.R
+import android.view.View
+import com.example.happybirthday.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
