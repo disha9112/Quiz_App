1e16c946b8fc4ca9b2e5f81628e185164c369684
disha9112 <78133928+disha9112@users.noreply.github.com>
2021-04-18 13:13:54 +0530

    Update ResultActivity.kt

--- app/src/main/java/com/example/happybirthday/ResultActivity.kt
+++ app/src/main/java/com/example/happybirthday/ResultActivity.kt
@@ -1,11 +1,27 @@
 package com.example.happybirthday
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val username = intent.getStringArrayExtra(Constants.USER_NAME)
+
+        tv_name.text = username.toString() //doubt to remove toString()
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswer = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tv_score.text = "Your Score is $correctAnswer out of $totalQuestions"
+
+        btn_finish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
