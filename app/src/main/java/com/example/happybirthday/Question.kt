ddd17247fd8deb4830968cd42188b27e6693984e
disha9112 <78133928+disha9112@users.noreply.github.com>
2021-04-18 13:12:14 +0530

    Update Question.kt

--- app/src/main/java/com/example/happybirthday/Question.kt
+++ app/src/main/java/com/example/happybirthday/Question.kt
@@ -1,15 +1,15 @@
 package com.example.happybirthday
 
 data class Question (
-    val id: Int,
-    val question: String,
-    val image: Int,
-    val optionOne: String,
-    val optionTwo: String,
-    val optionThree: String,
-    val optionFour: String,
-    val correctAnswer40: Int,
-    val correctAnswer30: Int,
-    val correctAnswer20: Int,
-    val correctAnswer10: Int
+        val id: Int,
+        val question: String,
+        val image: Int,
+        val optionOne: String,
+        val optionTwo: String,
+        val optionThree: String,
+        val optionFour: String,
+        val correctAnswer40: Int,
+        val correctAnswer30: Int,
+        val correctAnswer20: Int,
+        val correctAnswer10: Int
 )
